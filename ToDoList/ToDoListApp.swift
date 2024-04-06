e399336a6e46d0af86b71db3fbac6805851af370
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-04-06 11:01:20 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Veda Niav Cunniffe on 2024-04-06.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
