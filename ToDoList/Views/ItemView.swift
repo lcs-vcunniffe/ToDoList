7e5f1094d070fec0a761de23d0c84852619b2e0d
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-04-08 11:59:27 -0400

    Put ItemView into its own file.

---
+++ ToDoList/Views/ItemView.swift
@@ -0,0 +1,30 @@
+//
+//  ItemView.swift
+//  ToDoList
+//
+//  Created by Veda Niav Cunniffe on 2024-04-08.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    let title: String
+    let done: Bool
+    
+    var body: some View {
+        Label(
+            title: {
+                Text(title)
+            }, icon: {
+                Image(systemName: done == true ? "checkmark.circle" : "circle")
+            }
+        )
+    }
+}
+
+#Preview {
+    List {
+        ItemView(title: "Have a nap", done: false)
+        ItemView(title: "Eat lunch", done: true)
+    }
+}
--- ToDoList/Views/LandingView.swift
+++ ToDoList/Views/LandingView.swift
@@ -54,19 +54,3 @@ struct LandingView: View {
 #Preview {
     LandingView()
 }
-
-struct ItemView: View {
-    
-    let title: String
-    let done: Bool
-    
-    var body: some View {
-        Label(
-            title: {
-                Text(title)
-            }, icon: {
-                Image(systemName: done == true ? "checkmark.circle" : "circle")
-            }
-        )
-    }
-}
